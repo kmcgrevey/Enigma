a00f34e6d0192f8adaa67b68c372f00ac9b91853
Kevin McGrevey <niv3kmcg@gmail.com>
2020-01-11 16:29:15 -0700

    Create method to generate key_index code

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -1,3 +1,8 @@
 class Shifter
 
+  def key_index(key)
+    # require "pry"; binding.pry
+    { :a => key[0..1], :b => key[1..2], :c => key[2..3], :d => key[3..4]}
+  end
+
 end
--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -6,10 +6,22 @@ class ShifterTest < Minitest::Test
 
   def setup
     @shifter = Shifter.new
+    # @message = Enigma.new.encrypt("hello world", "02715", "040895")
   end
 
   def test_it_exists
     assert_instance_of Shifter, @shifter
+  end
+
+  def test_it_can_create_a_key_index
+    # assert_equal Hash.new(0), @shifter.key_index
+    # message = mock('enigma')
+
+    # message.stubs(:key).returns("02715")
+
+    expected = { :a => "02", :b => "27", :c => "71", :d => "15"}
+
+    assert_equal expected, @shifter.key_index("02715")
   end
 
 end
