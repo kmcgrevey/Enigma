b7f9991e1f9897fe2d222aaf171f7348379c3068
Kevin McGrevey <niv3kmcg@gmail.com>
2020-01-12 16:42:33 -0700

    Repair functionality after removal of indexer methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,30 +9,14 @@ class Enigma
     @message = message
     @enig_key = enig_key
     @date = date
-    @k_i = key_index(enig_key)
-    @o_i = offset_index(date)
-    @shift_index = shift_indexer(@k_i, @o_i)
+    # @k_i = key_index(enig_key)
+    # @o_i = offset_index(date)
+    # @shift_index = shift_indexer(@k_i, @o_i)
   end
 
-  def key_index(enig_key)
-    { :a => enig_key[0..1],
-      :b => enig_key[1..2],
-      :c => enig_key[2..3],
-      :d => enig_key[3..4]}
-  end
 
-  def offset_index(date)
-    date_squared = (date.to_i * date.to_i).to_s
-    { :a => date_squared[-4],
-      :b => date_squared[-3],
-      :c => date_squared[-2],
-      :d => date_squared[-1],}
-  end
 
-  def shift_indexer(enig_key, offset)
-    enig_key.merge(offset) do |key, enig_key_val, offset_val|
-      enig_key_val.to_i + offset_val.to_i
-    end
-  end
+
+
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -42,32 +42,4 @@ class EnigmaTest < Minitest::Test
     assert_equal "i shall return", @enigma.message
   end
 
-  def test_it_can_create_a_key_index
-    expected = { :a => "02",
-                :b => "27",
-                :c => "71",
-                :d => "15"}
-
-    assert_equal expected, @enigma.key_index("02715")
-  end
-
-  def test_it_can_create_an_offset_index
-    expected = { :a => "1",
-                :b => "0",
-                :c => "2",
-                :d => "5"}
-
-    assert_equal expected, @enigma.offset_index("040895")
-  end
-
-  def test_it_creates_the_shift_key
-    @enigma.encrypt("hello world", "02715", "040895")
-    expected = { :a => 3,
-                :b => 27,
-                :c => 73,
-                :d => 20 }
-
-    assert_equal expected, @enigma.shift_index
-  end
-
 end
