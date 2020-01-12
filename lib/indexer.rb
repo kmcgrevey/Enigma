653af0ddd1f6ff2dce2e2e9718e115dbc0a02d23
Kevin McGrevey <niv3kmcg@gmail.com>
2020-01-12 13:30:30 -0700

    Migrate key_index and offset_index into Indexer

--- lib/indexer.rb
+++ lib/indexer.rb
@@ -1,4 +1,25 @@
 class Indexer
+  attr_reader :enig_key, :date
 
+  def initialize(enig_key, date)
+    @enig_key = enig_key
+    @date = date
+  end
+
+  def key_index(enig_key)
+    { :a => enig_key[0..1],
+      :b => enig_key[1..2],
+      :c => enig_key[2..3],
+      :d => enig_key[3..4]}
+  end
+
+
+  def offset_index(date)
+    date_squared = (date.to_i * date.to_i).to_s
+    { :a => date_squared[-4],
+      :b => date_squared[-3],
+      :c => date_squared[-2],
+      :d => date_squared[-1],}
+  end
 
 end
--- test/indexer_test.rb
+++ test/indexer_test.rb
@@ -5,7 +5,34 @@ require_relative '../lib/indexer'
 class IndexerTest < Minitest::Test
 
   def setup
-    @indexer = Enigma.new("02715", "040895")
+    @indexer = Indexer.new("02715", "040895")
+  end
+
+  def test_it_exists
+    assert_instance_of Indexer, @indexer
+  end
+
+  def test_it_has_attributes
+    assert_equal "02715", @indexer.enig_key
+    assert_equal "040895", @indexer.date
+  end
+
+  def test_it_can_create_a_key_index
+    expected = { :a => "02",
+                :b => "27",
+                :c => "71",
+                :d => "15"}
+
+    assert_equal expected, @indexer.key_index("02715")
+  end
+
+  def test_it_can_create_an_offset_index
+    expected = { :a => "1",
+                :b => "0",
+                :c => "2",
+                :d => "5"}
+
+    assert_equal expected, @indexer.offset_index("040895")
   end
 
 end
