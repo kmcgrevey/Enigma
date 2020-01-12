9a039849067096537c5050b09bd0da44b0a8e3c3
Kevin McGrevey <niv3kmcg@gmail.com>
2020-01-11 17:09:48 -0700

    Remove key_index functionality

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -1,8 +1,5 @@
 class Shifter
 
-  def key_index(key)
-    # require "pry"; binding.pry
-    { :a => key[0..1], :b => key[1..2], :c => key[2..3], :d => key[3..4]}
-  end
+  
 
 end
--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -13,15 +13,6 @@ class ShifterTest < Minitest::Test
     assert_instance_of Shifter, @shifter
   end
 
-  def test_it_can_create_a_key_index
-    # assert_equal Hash.new(0), @shifter.key_index
-    # message = mock('enigma')
-
-    # message.stubs(:key).returns("02715")
-
-    expected = { :a => "02", :b => "27", :c => "71", :d => "15"}
-
-    assert_equal expected, @shifter.key_index("02715")
-  end
+  
 
 end
