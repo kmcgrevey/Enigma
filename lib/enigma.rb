bc2a7426b7a027092b3d137814b2247f0cc66994
Kevin McGrevey <niv3kmcg@gmail.com>
2020-01-14 19:11:58 -0700

    Enable function to write encrypted text file

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -12,16 +12,24 @@ class Enigma
     @date = date
 
     @shift_index = Indexer.new.generate_shift_index(enig_key, date)
-    encrypt_msg = Shifter.new.encrypt_shift(message, @shift_index)
-    encrypted_data = { encryption: encrypt_msg, key: enig_key, date: date }
+    @encrypt_msg = Shifter.new.encrypt_shift(message, @shift_index)
+    encrypted_data = { encryption: @encrypt_msg, key: enig_key, date: date }
 
-    puts "Created #{ARGV[1]} with the key #{enig_key} and date #{date}"
+    generate_output
   end
 
   def decrypt(message, enig_key, date)
     shift_index = Indexer.new.generate_shift_index(enig_key, date)
     decrypt_msg = Shifter.new.decrypt_shift(message, shift_index)
     { decryption: decrypt_msg, key: enig_key, date: date }
+  end
+
+  def generate_output
+    puts "Created '#{ARGV[1]}' with the key #{enig_key} and date #{date}"
+
+    message = File.open(ARGV[1], "w")
+    message.write(@encrypt_msg)
+    message.close
   end
 
 
